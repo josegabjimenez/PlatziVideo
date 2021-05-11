6c090087cdba935c8d3907a4b222dfe507636a49
josegabjimenez <jose.vidales@correounivalle.edu.co>
2021-05-10 22:22:37 -0500

    ESLint

---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../assets/styles/App.scss"
+
+function HolaMundo() {
+    return (
+        <div>
+            <h1>Hello world! this is a proof</h1>
+        </div>
+    )
+}
+
+export default HolaMundo
